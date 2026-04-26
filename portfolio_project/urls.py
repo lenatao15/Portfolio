367f9679b172d3ec4e0f71094dbfba6e630320f8
Lena <lenatao15@gmail.com>
2026-04-25 20:31:07 -0500

    Configure media serving and enhance project detail UI

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # On Render, we can use this to serve media files if we're not using a persistent disk
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
